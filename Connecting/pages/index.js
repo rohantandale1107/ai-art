ef3931cf9473befee21eef302a9fa153146294fe
rohantandale1107 <rohanhtandale11@gmail.com>
2024-10-01 16:42:29 +0530

    commit

--- Connecting/pages/index.js
+++ Connecting/pages/index.js
@@ -201,7 +201,7 @@ const index = () => {
           <GetStarted activeUser={activeUser} />
           <div className="w-screen overflow-x-hidden flex flex-col items-center py-4 mt-16">
             <a href="/">
-            <HomeLogo/>
+              <HomeLogo />
             </a>
             <a href="/aperture" className="cursor-pointer">
               <p className="mt-2 text-xs text-indigo-300 active:scale-95 text-center font-medium shadow-sm hover:shadow-md bg-indigo-300 bg-opacity-5 hover:bg-opacity-10 border border-indigo-300 border-opacity-10 hover:border-opacity-20 transition-all rounded-md px-6 py-2 ">
@@ -210,56 +210,115 @@ const index = () => {
             </a>
             <div className="flex flex-items-center w-full max-w-[600px] md:ml-[48px] mt-8 px-4 pl-5 md:px-5">
               <div className="w-full">
-                <div className="w-full flex items-center relative" 
-                  onClick={()=> changeCategory("Filter")}>
-                  <Search/>
+                <div
+                  className="w-full flex items-center relative"
+                  onClick={() => changeCategory("Filter")}
+                >
+                  <Search />
                   <input
                     className="bg-zinc-700 flex-1 pl-12 pr-12 rounded-full text-sm px-4 py-2.5 focus:outline-none focus:ring-1 focus:ring-indigo-700"
                     placeholder="Search for an image"
-                    onChange={(e)=> setSearchItem(e.target.value)}
+                    onChange={(e) => setSearchItem(e.target.value)}
                     value={searchItem}
-                    />
+                  />
 
                   <button
-                    
                     type="button"
                     className="text-base absolute right-2 hover:bg-zinc-800 h-8 w-8 flex tiems-center justify-center rounded-full"
-                    >
-                      <Image/>
+                  >
+                    <Image />
                   </button>
-                  
-                  
                 </div>
               </div>
               <div className="flex justify-center ">
                 <button
-                onClick={()=>
-                  openFilter ? setOpenFilter(false) : setOpenFilter(true)
-                }
-                type="button"
-                className="ml-2 h-10 w-10 rounded-full cursor-pointer flex items-center justify-center bg-transparent hover:bg-zinc-900"
+                  onClick={() =>
+                    openFilter ? setOpenFilter(false) : setOpenFilter(true)
+                  }
+                  type="button"
+                  className="ml-2 h-10 w-10 rounded-full cursor-pointer flex items-center justify-center bg-transparent hover:bg-zinc-900"
                 >
-                  <Filter/>
+                  <Filter />
                 </button>
-                
               </div>
             </div>
-            <div className="flex w-full max-w-[600px] md:ml-[48px] px-4 pl-5 md:px-5"
+            <div
+              className="flex w-full max-w-[600px] md:ml-[48px] px-4 pl-5 md:px-5"
               style={{
-              position:"relative",
-              }}>
-              {openFilter &&<Notic/>}
+                position: "relative",
+              }}
+            >
+              {openFilter && <Notic />}
               <div className="mb-8 flex flex-col items-center">
                 <div className="flex space-x-2">
                   <button className="w-32 sm:w-36 flex items-center text-xs justify-center text-center h-9 rounded-full hover:brightness-110 bg-opacity-0 shadow-sm mt-4 bg-gradient-to-t from-indigo-900 via-indigo-900 to-indigo-800">
                     Search
                   </button>
+                  <a href="/aperture">
+                    <button className="w-32 sm:w-36 h-9 mt-4 flex items-center justify-center text-xs text-center rounded-full border border-gray-700 shadow-sm bg-opacity-0 hover:bg-zinc-700 hover:brightness-110">
+                      Generate
+                    </button>
+                  </a>
                 </div>
               </div>
+              <div className="flex space-x-2 px-2">
+                <Button
+                  icon={<BsCameraReelsFill />}
+                  name={"Reel"}
+                  handleClick={() => changeCategory("Reel")}
+                  category={category}
+                />
+                <Button
+                  icon={<AiOutLineYoutube />}
+                  name={"Youtube"}
+                  handleClick={() => changeCategory("Youtube")}
+                  category={category}
+                />
+                <Button
+                  icon={<FaInstagram />}
+                  name={"Instagram"}
+                  handleClick={() => changeCategory("Instagram")}
+                  category={category}
+                />
               </div>
+              <div className="mt-2">&nbsp;</div>
+              <div className="mt-3 relative px-2 md:px-7 w-full">
+                <div
+                  className="active:outline-none focus:outline-none overflow-hidden new-css-style-box"
+                  role="grid"
+                  tabIndex={0}
+                  style={{
+                    position: "relative",
+                    display: "grid",
+                    gridTemplateColumns: "repeat(auto-fill,minmax(200px,1fr))",
+                    listStyle: "none",
+                    margin: "0",
+                    padding: "0",
+                    gap: ".1rem",
+                  }}
+                >
+                  <>
+                    {arrayRender?.reverse().map((item, index) => (
+                      <ImageCard
+                        index={index}
+                        item={item}
+                        setSingleID={setSingleID}
+                        activeUser={activeUser}
+                      />
+                    ))}
+                  </>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+
+      {singleID && (
+        <SingleImage singleID={singleID} setSingleID={setSingleID} />
+      )}
+
+      {buying && <PaymentProcessing buying={buying} setBuying={setBuying} />}
     </div>
   );
 };
